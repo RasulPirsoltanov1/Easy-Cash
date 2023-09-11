d49f0054428218ceb787cad3014c7c280c2af6ff
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-09-11 20:16:06 +0400

    added Dtos and relations

--- EasyCashIdentityProject.DataAccessLayer/Migrations/20230911150940_add_relation_CustomerAccount_and_AppUser.Designer.cs
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230911150940_add_relation_CustomerAccount_and_AppUser.Designer.cs
@@ -4,6 +4,7 @@ using EasyCashIdentityProject.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyCashIdentityProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230911150940_add_relation_CustomerAccount_and_AppUser")]
+    partial class add_relation_CustomerAccount_and_AppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -12,5 +12,7 @@ namespace EasyCashIdentityProject.EntityLayer.Concrete
         public string AccountNumber { get; set; }
         public string Currency { get; set; }
         public decimal Balance{ get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
